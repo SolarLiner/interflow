7f181b30b18170d1c34dce6373403d09f4c4744e
Nathan Graule <solarliner@gmail.com>
2025-03-15 22:46:40 +0100

    wip: duplex mode API + implementation for ALSA

--- src/backends/alsa/mod.rs
+++ src/backends/alsa/mod.rs
@@ -5,14 +5,15 @@
 //! (PulseAudio, PipeWire) offer ALSA-compatible APIs so that older software can still access the
 //! audio devices through them.
 
-use crate::device::DeviceType;
+use crate::{device::DeviceType, driver::AudioDuplexDriver};
 use crate::driver::AudioDriver;
 use alsa::device_name::HintIter;
-use device::AlsaDevice;
+use device::{AlsaDevice, AlsaDuplexDevice};
 use std::borrow::Cow;
 use thiserror::Error;
 
 mod device;
+mod duplex;
 mod input;
 mod output;
 mod stream;
@@ -52,5 +53,34 @@ impl AudioDriver for AlsaDriver {
     fn list_devices(&self) -> Result<impl IntoIterator<Item = Self::Device>, Self::Error> {
         Ok(HintIter::new(None, c"pcm")?
             .filter_map(|hint| AlsaDevice::new(hint.name.as_ref()?, hint.direction?).ok()))
+    }
+}
+
+impl AudioDuplexDriver for AlsaDriver {
+    type DuplexDevice = AlsaDuplexDevice;
+
+    fn default_duplex_device(&self) -> Result<Option<Self::DuplexDevice>, Self::Error> {
+        let Some(input) = self.default_device(DeviceType::Input)? else {
+            return Ok(None);
+        };
+        let Some(output) = self.default_device(DeviceType::Output)? else {
+            return Ok(None);
+        };
+        Ok(Some(AlsaDuplexDevice::new(input, output)))
+    }
+
+    fn list_duplex_devices(
+        &self,
+    ) -> Result<impl IntoIterator<Item = Self::DuplexDevice>, Self::Error> {
+        Ok(HintIter::new(None, c"pcm")?
+            .filter_map(|hint| AlsaDuplexDevice::full_duplex(hint.name.as_ref()?).ok()))
+    }
+
+    fn device_from_input_output(
+        &self,
+        input: Self::Device,
+        output: Self::Device,
+    ) -> Result<Self::DuplexDevice, Self::Error> {
+        Ok(AlsaDuplexDevice::new(input, output))
     }
 }
