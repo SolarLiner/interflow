9d08a326177be06565707e04d875eb6f4b74f20e
Nathan Graule <solarliner@gmail.com>
2025-03-16 14:18:56 +0100

    fix(alsa): wrong channel map in stream config sent to callback

--- src/backends/alsa/stream.rs
+++ src/backends/alsa/stream.rs
@@ -69,8 +69,7 @@ impl<Callback: 'static + Send> AlsaStream<Callback> {
                 log::info!("Sample rate : {samplerate}");
                 let stream_config = StreamConfig {
                     samplerate,
-                    channels: ChannelMap32::default()
-                        .with_indices(std::iter::repeat(1).take(num_channels)),
+                    channels: ChannelMap32::default().with_indices(0..num_channels),
                     buffer_size_range: (Some(period_size), Some(period_size)),
                     exclusive: false,
                 };
