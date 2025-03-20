ac0ac844e7e5bf1c6a4be8c04cc769b65b69cf57
Dr George Atkinson <geomyles@yahoo.co.uk>
2025-03-20 16:26:34 +0000

    Add timestamps

--- src/backends/asio/prelude.rs
+++
@@ -1,1 +0,0 @@
-pub use super::{device::AsioDevice, driver::AsioDriver, error::AsioError, stream::AsioStream};
--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -109,5 +109,5 @@ pub fn default_output_device() -> impl AudioOutputDevice {
     // #[cfg(os_wasapi)]
     // return default_output_device_from(&wasapi::WasapiDriver);
     #[cfg(os_asio)]
-    return default_output_device_from(&asio::driver::AsioDriver::new().unwrap());
+    return default_output_device_from(&asio::AsioDriver::new().unwrap());
 }
