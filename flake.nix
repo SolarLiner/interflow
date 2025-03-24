cacce0111f7833b88a83f2944c4b2848cee88f53
Nathan Graule <solarliner@gmail.com>
2025-03-24 22:55:55 +0100

    chore: add nix flake + direnv integration + github actions for nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { stdenv = pkgs.clangStdenv; };
+        nativeBuildInputs = with pkgs; [pkg-config];
+        buildInputs = with pkgs; [clangStdenv.cc.libc alsa-lib pipewire jack2];
+        LIBCLANG_PATH = with pkgs; "${llvmPackages.libclang.lib}/lib";
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          pname = "interflow";
+          version = "0.1.0";
+          src = ./.;
+          inherit nativeBuildInputs buildInputs LIBCLANG_PATH;
+        };
+        devShells.default = pkgs.clangStdenv.mkDerivation {
+          name = "interflow-devshell";
+          buildInputs = buildInputs ++ nativeBuildInputs;
+          inherit LIBCLANG_PATH;
+        };
+      }
+    );
+}
