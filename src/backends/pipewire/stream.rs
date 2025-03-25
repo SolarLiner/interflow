1456cbcf78d7616485c088065c686d39da23ab7b
Nathan Graule <solarliner@gmail.com>
2025-03-25 16:10:02 +0100

    wip(pipewire): output callback now works properly

--- src/backends/pipewire/stream.rs
+++ src/backends/pipewire/stream.rs
@@ -1,4 +1,4 @@
-use crate::audio_buffer::AudioMut;
+use crate::audio_buffer::{AudioBuffer, AudioMut};
 use crate::backends::pipewire::error::PipewireError;
 use crate::channel_map::Bitset;
 use crate::timestamp::Timestamp;
@@ -10,7 +10,7 @@ use libspa::param::audio::{AudioFormat, AudioInfoRaw};
 use libspa::pod::Pod;
 use libspa_sys::{SPA_PARAM_EnumFormat, SPA_TYPE_OBJECT_Format};
 use pipewire::context::Context;
-use pipewire::main_loop::MainLoop;
+use pipewire::main_loop::{MainLoop, WeakMainLoop};
 use pipewire::properties::properties;
 use pipewire::stream::{Stream, StreamFlags, StreamListener};
 use pipewire::{keys, Error};
@@ -36,9 +36,9 @@ struct StreamInner<Callback> {
     commands: rtrb::Consumer<StreamCommands<Callback>>,
     scratch_buffer: Box<[f32]>,
     callback: Option<Callback>,
-    ejected: bool,
     config: StreamConfig,
     timestamp: Timestamp,
+    loop_ref: WeakMainLoop,
 }
 
 impl<Callback> StreamInner<Callback> {
@@ -52,7 +52,9 @@ impl<Callback> StreamInner<Callback> {
             StreamCommands::Eject(reply) => {
                 if let Some(callback) = self.callback.take() {
                     reply.send(callback).unwrap();
-                    self.ejected = true;
+                    if let Some(loop_ref) = self.loop_ref.upgrade() {
+                        loop_ref.quit();
+                    }
                 }
             }
         }
@@ -63,22 +65,34 @@ impl<Callback> StreamInner<Callback> {
             self.handle_command(command);
         }
     }
+
+    fn ejected(&self) -> bool {
+        self.callback.is_none()
+    }
 }
 
 impl<Callback: AudioOutputCallback> StreamInner<Callback> {
-    fn process(&mut self, buffer: AudioMut<f32>) {
+    fn process(&mut self, channels: usize, frames: usize) -> usize {
+        let buffer = AudioMut::from_noninterleaved_mut(
+            &mut self.scratch_buffer[..channels * frames],
+            channels,
+        )
+        .unwrap();
         if let Some(callback) = self.callback.as_mut() {
             let context = AudioCallbackContext {
                 stream_config: self.config,
                 timestamp: self.timestamp,
             };
-            let num_frames = buffer.num_samples() as u64;
+            let num_frames = buffer.num_samples();
             let output = AudioOutput {
                 buffer,
                 timestamp: self.timestamp,
             };
             callback.on_output_data(context, output);
-            self.timestamp += num_frames;
+            self.timestamp += num_frames as u64;
+            num_frames
+        } else {
+            0
         }
     }
 }
@@ -102,7 +116,10 @@ impl<Callback> AudioStreamHandle<Callback> for StreamHandle<Callback> {
     }
 }
 
+const MAX_FRAMES: usize = 8192;
+
 impl<Callback: 'static + Send + AudioOutputCallback> StreamHandle<Callback> {
+    /// Create a Pipewire stream
     pub fn new(
         name: impl ToString,
         mut config: StreamConfig,
@@ -132,30 +149,46 @@ impl<Callback: 'static + Send + AudioOutputCallback> StreamHandle<Callback> {
                 .add_local_listener_with_user_data(StreamInner {
                     callback: None,
                     commands: rx,
-                    scratch_buffer: vec![0.0; channels * 8192].into_boxed_slice(),
-                    ejected: false,
+                    scratch_buffer: vec![0.0; MAX_FRAMES * channels].into_boxed_slice(),
+                    loop_ref: main_loop.downgrade(),
                     config,
                     timestamp: Timestamp::new(config.samplerate),
                 })
                 .process(move |stream, inner| {
                     log::debug!("Processing stream");
                     inner.handle_commands();
-                    if inner.ejected {
-                        if let Err(err) = stream.disconnect() {
-                            log::error!("Failed to disconnect stream: {}", err);
-                        }
+                    if inner.ejected() {
                         return;
                     }
                     if let Some(mut buffer) = stream.dequeue_buffer() {
                         let datas = buffer.datas_mut();
-                        let buf = &mut datas[0];
-                        if let Some(data) = buf.data() {
-                            let data: &mut [f32] = zerocopy::FromBytes::mut_from_bytes(data)
-                                .expect("Pipewire buffer cannot be cast to f32 slice");
-                            let buffer = AudioMut::from_noninterleaved_mut(data, channels)
-                                .expect("Pipewire buffer is incomplete");
-                            inner.process(buffer);
+                        log::debug!("Datas: len={}", datas.len());
+                        let Some(min_frames) = datas
+                            .iter_mut()
+                            .filter_map(|d| d.data().map(|d| d.len() / size_of::<f32>()))
+                            .min()
+                        else {
+                            log::warn!("No datas available");
+                            return;
+                        };
+                        let frames = min_frames.min(MAX_FRAMES);
+                        let frames = inner.process(channels, frames);
+
+                        for (i, data) in datas.iter_mut().enumerate() {
+                            let processed_slice = &mut inner.scratch_buffer[i * frames..][..frames];
+                            if let Some(data) = data.data() {
+                                let slice: &mut [f32] = zerocopy::FromBytes::mut_from_bytes(data)
+                                    .inspect_err(|e| log::error!("Cannot cast to f32 slice: {e}"))
+                                    .unwrap();
+                                slice[..frames].copy_from_slice(processed_slice);
+                            }
+                            let chunk = data.chunk_mut();
+                            *chunk.offset_mut() = 0;
+                            *chunk.stride_mut() = size_of::<f32>() as _;
+                            *chunk.size_mut() = (size_of::<f32>() * frames) as _;
                         }
+                    } else {
+                        log::warn!("No buffer available");
                     }
                 })
                 .register()?;
@@ -184,7 +217,7 @@ impl<Callback: 'static + Send + AudioOutputCallback> StreamHandle<Callback> {
             )?;
             log::debug!("Starting Pipewire main loop");
             main_loop.run();
-            return Ok::<_, PipewireError>(());
+            Ok::<_, PipewireError>(())
         });
         log::debug!("Sending callback to stream");
         tx.push(StreamCommands::ReceiveCallback(callback)).unwrap();
