245c1d8f8492bdaf89d19b2e93e8fa41e55c3a4d
Nathan Graule <nathan.graule@arturia.com>
2026-07-23 15:45:48 +0200

    feat(core): register mutable references in ExtensionProvider, and automatically register Self as an available type to look up

--- crates/interflow-core/src/traits.rs
+++ crates/interflow-core/src/traits.rs
@@ -2,7 +2,7 @@ use std::any::TypeId;
 use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 
-/// A fully type-erased pointer, that can work with both thin and fat pointers.
+/// A fully type-erased pointer that can work with both thin and fat pointers.
 /// Copied from <https://users.rust-lang.org/t/type-erasing-pointers-to-t-sized/96984>.
 #[derive(Copy, Clone)]
 struct ErasedPtr {
@@ -30,14 +30,47 @@ impl ErasedPtr {
         res
     }
 
+    /// Erase `ptr`.
+    fn new_mut<T: ?Sized>(ptr: *mut T) -> Self {
+        let mut res = ErasedPtr {
+            value: MaybeUninit::zeroed(),
+        };
+
+        let len = size_of::<*const T>();
+        assert!(len <= size_of::<[usize; 2]>());
+
+        let ptr_val = (&ptr) as *const *mut T as *mut u8;
+        let target = res.value.as_mut_ptr() as *mut u8;
+        // SAFETY: The target is valid for at least `len` bytes, and has no
+        // requirements on the value.
+        unsafe {
+            core::ptr::copy_nonoverlapping(ptr_val, target, len);
+        }
+
+        res
+    }
+
     /// Convert the type erased pointer back into a pointer.
     ///
     /// # Safety
     ///
-    /// The type `T` must be the same type as the one used with `new`.
+    /// The type `T` must be the same type as the one used with `new` or `new_mut`.
     unsafe fn as_ptr<T: ?Sized>(&self) -> *const T {
         // SAFETY: The constructor ensures that the first `size_of::<T>()`
         // bytes of `&self.value` are a valid `*const T` pointer.
+        unsafe { core::mem::transmute_copy(&self.value) }
+    }
+
+    /// Convert the type erased pointer back into a pointer.
+    ///
+    /// # Safety
+    ///
+    /// This [`ErasedPtr`] must have been constructed from [`Selector::new_mut`], using the same `T` type used in
+    /// this function.
+    unsafe fn as_mut_ptr<T: ?Sized>(&self) -> *mut T {
+        // SAFETY: The constructor ensures that the first `size_of::<T>()`
+        // bytes of `&self.value` are a valid `*const T` pointer.
+        // The caller ensures that the original pointer was mutable.
         unsafe { core::mem::transmute_copy(&self.value) }
     }
 }
@@ -50,6 +83,7 @@ pub struct Selector<'a> {
     __lifetime: PhantomData<&'a ()>,
     target: TypeId,
     found: Option<ErasedPtr>,
+    found_mut: Option<ErasedPtr>,
 }
 
 impl<'a> Selector<'a> {
@@ -58,6 +92,7 @@ impl<'a> Selector<'a> {
             __lifetime: PhantomData,
             target: TypeId::of::<T>(),
             found: None,
+            found_mut: None,
         }
     }
 
@@ -68,9 +103,22 @@ impl<'a> Selector<'a> {
         self
     }
 
+    pub fn register_mut<T: 'static + ?Sized>(&mut self, value: &mut T) -> &mut Self {
+        if self.target == TypeId::of::<T>() {
+            self.found_mut = Some(ErasedPtr::new_mut(value));
+        }
+        self
+    }
+
     pub(crate) fn finish<I: 'static + ?Sized>(self) -> Option<&'a I> {
         assert_eq!(self.target, TypeId::of::<I>());
-        Some(unsafe { &*self.found?.as_ptr() })
+        let target = self.found.or(self.found_mut)?;
+        Some(unsafe { &*target.as_ptr() })
+    }
+
+    pub(crate) fn finish_mut<I: 'static + ?Sized>(self) -> Option<&'a mut I> {
+        assert_eq!(self.target, TypeId::of::<I>());
+        Some(unsafe { &mut *self.found_mut?.as_mut_ptr() })
     }
 }
 
@@ -80,6 +128,12 @@ pub trait ExtensionProvider: 'static {
     /// function will be called for every request. Runtime checks are expected, but this function should remain as fast
     /// as possible.
     fn register<'a, 'sel>(&'a self, selector: &'sel mut Selector<'a>) -> &'sel mut Selector<'a>;
+    fn register_mut<'a, 'sel>(
+        &'a mut self,
+        selector: &'sel mut Selector<'a>,
+    ) -> &'sel mut Selector<'a> {
+        self.register(selector)
+    }
 }
 
 const _EXTENSION_TRAIT_ASSERTS: () = {
@@ -88,14 +142,25 @@ const _EXTENSION_TRAIT_ASSERTS: () = {
 };
 
 pub trait ExtensionProviderExt: ExtensionProvider {
-    /// Look up [`T`] from the extension if it is registered.
-    fn lookup<T: 'static + ?Sized>(&self) -> Option<&T> {
-        let mut selector = Selector::new::<T>();
-        {
-            self.register(&mut selector);
-        }
-        selector.finish::<T>()
-    }
+    /// Look up [`T`] from the extension if it is registered. Returns an immutable reference.
+    fn lookup<T: 'static + ?Sized>(&self) -> Option<&T>;
+
+    /// Look up [`T`] from the extension if it is registered. Returns as mutable reference.
+    fn lookup_mut<T: 'static + ?Sized>(&mut self) -> Option<&mut T>;
 }
 
-impl<E: ?Sized + ExtensionProvider> ExtensionProviderExt for E {}
+impl<E: ?Sized + ExtensionProvider> ExtensionProviderExt for E {
+    fn lookup<T: 'static + ?Sized>(&self) -> Option<&T> {
+        let mut selector = Selector::new::<T>();
+        selector.register::<E>(self);
+        self.register(&mut selector);
+        selector.finish::<T>()
+    }
+
+    fn lookup_mut<T: 'static + ?Sized>(&mut self) -> Option<&mut T> {
+        let mut selector = Selector::new::<T>();
+        selector.register_mut::<E>(self);
+        self.register_mut(&mut selector);
+        selector.finish_mut::<T>()
+    }
+}
